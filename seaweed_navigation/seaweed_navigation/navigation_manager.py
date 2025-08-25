92b3f756337e50414d9f7ecfe6b20b7ba1a05420
varunpat789 <varunpat789@gmail.com>
2025-08-25 14:24:11 -0400

    feat: implemented A* planner, navigation manager, and test launch scripts

---
+++ seaweed_navigation/seaweed_navigation/navigation_manager.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+import rclpy
+from rclpy.time import Time
+from rclpy.node import Node
+from rclpy.duration import Duration
+
+# from rclpy.action import ActionServer
+from geometry_msgs.msg import PoseStamped, Pose, Point
+from a_star import AStarPlanner
+from nav_msgs.msg import OccupancyGrid, Path
+from tf2_ros import Buffer, TransformListener
+from rclpy.qos import QoSProfile, DurabilityPolicy
+
+
+class NavigationManager(Node):
+    def __init__(self):
+        super().__init__("navigation_manager")
+
+        # self.action_server = ActionServer(
+        #     self,
+        #     NavigateToPose,
+        #     'navigate_to_pose',
+        #     execute_callback=self.execute_callback,
+        #     cancel_callback=self.cancel_callback,
+        #     goal_callback=self.goal_callback
+        # )
+
+        map_qos = QoSProfile(depth=10)
+        map_qos.durability = DurabilityPolicy.TRANSIENT_LOCAL
+
+        self.map_sub = self.create_subscription(OccupancyGrid, "/map", self.map_callback, map_qos)
+        self.pose_sub = self.create_subscription(PoseStamped, "/goal_pose", self.goal_callback, 10)
+
+        self.path_pub = self.create_publisher(Path, "/path", 10)
+
+        self.map = None
+        goal_tol = 0.1  # m
+        self.planner = AStarPlanner(map=self.map, goal_tolerance=goal_tol)
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        # DEBUG: Visualize visited nodes
+        self.debug_map = OccupancyGrid()
+        self.debug_map_pub = self.create_publisher(OccupancyGrid, "/debug/map", 10)
+
+    def map_callback(self, map_msg: OccupancyGrid):
+        self.map = map_msg
+        self.planner.map = map_msg
+
+        self.debug_map.header.frame_id = map_msg.header.frame_id
+        self.debug_map.info = map_msg.info
+
+    def goal_callback(self, goal_pose_stamped: PoseStamped):
+        if self.map is None:
+            self.get_logger().error("No map received")
+            return
+
+        base_link_frame = "wamv/base_link"
+        robot_pose = self.lookup_pose(target_frame=self.map.header.frame_id, source_frame=base_link_frame)
+
+        if robot_pose is None:
+            self.get_logger().error("Can't get robot pose in map frame")
+            return
+
+        robot_position = Point()
+        robot_position.x = robot_pose.position.x
+        robot_position.y = robot_pose.position.y
+
+        goal_point = Point()
+        goal_point.x = goal_pose_stamped.pose.position.x
+        goal_point.y = goal_pose_stamped.pose.position.y
+
+        path = self.planner.plan(robot_position, goal_point)
+
+        if path and path.poses:
+            self.get_logger().info("Path found")
+            self.path_pub.publish(path)
+        else:
+            self.get_logger().warn("No path found")
+
+    def lookup_pose(self, target_frame: str, source_frame: str) -> Optional[Pose]:
+        try:
+            tf = self.tf_buffer.lookup_transform(target_frame, source_frame, Time(), timeout=Duration(seconds=1))
+
+            pose = Pose()
+            pose.position.x = tf.transform.translation.x
+            pose.position.y = tf.transform.translation.y
+            pose.position.z = tf.transform.translation.z
+            pose.orientation = tf.transform.rotation
+
+            return pose
+
+        except Exception as ex:
+            self.get_logger().error(f"Cannot transform from {source_frame} to {target_frame}: {ex}")
+            return None
+
+
+def main(args=None):  # type: ignore
+    rclpy.init(args=args)
+    try:
+        navigation_manager = NavigationManager()
+        rclpy.spin(navigation_manager)
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ seaweed_navigation/seaweed_navigation/test_map_generator.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import OccupancyGrid
+from tf2_ros import Buffer, TransformListener, TransformStamped
+from rclpy.qos import QoSProfile, DurabilityPolicy
+from tf2_ros import StaticTransformBroadcaster
+from enum import Enum
+
+
+class Direction(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
+
+
+class TestMapGenerator(Node):
+    def __init__(self):
+        super().__init__("test_map")
+
+        map_qos = QoSProfile(depth=10)
+        map_qos.durability = DurabilityPolicy.TRANSIENT_LOCAL
+
+        self.map_pub = self.create_publisher(OccupancyGrid, "/map", map_qos)
+        self.map = OccupancyGrid()
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.tf_broadcaster = StaticTransformBroadcaster(self)
+
+        # map params
+        self.map_width = 700
+        self.map_height = 700
+        self.map_resolution = 0.05
+        self.map_origin = [-20.0, -20.0, 0.0]  # keep z = 0.0
+
+        self.init_map()
+
+        # reference: Unknown = -1, Obstacles = 100, 0 < Free < 99
+        self.grid = np.full((self.map_height, self.map_width), 0, dtype=np.int8)
+
+        # ADD TEST OBSTACLES HERE
+        # self.generate_rect(1, 1, 5, 5)
+        # self.generate_rect(0, 0, 10, 10)
+
+        # self.publish_test_robot_tf()
+        self.generate_map_timer = self.create_timer(0.1, self.timer_callback)
+
+    def init_map(self):
+        self.map.header.frame_id = "map"
+        self.map.info.resolution = self.map_resolution
+        self.map.info.width = self.map_width
+        self.map.info.height = self.map_height
+        self.map.info.origin.position.x = self.map_origin[0]
+        self.map.info.origin.position.y = self.map_origin[1]
+        self.map.info.origin.position.z = self.map_origin[2]
+        self.map.info.origin.orientation.w = 1.0
+
+    def generate_circle(self, x: int, y: int, r: int):
+        pass
+
+    def generate_line(self, x: int, y: int, dist: int, direction: Direction):
+        if direction == Direction.HORIZONTAL:
+            for i in range(dist):
+                self.grid[x + i, y] = 100
+        elif direction == Direction.VERTICAL:
+            for i in range(dist):
+                self.grid[x, y + i] = 100
+
+    def generate_rect(self, x: int, y: int, l: int, w: int):
+        # l -> horizontal, w -> vertical
+        self.generate_line(x, y, l, Direction.HORIZONTAL)
+        self.generate_line(x, y + w, l, Direction.HORIZONTAL)
+        self.generate_line(x, y, l, Direction.VERTICAL)
+        self.generate_line(x + l, y, l, Direction.VERTICAL)
+
+    def publish_test_robot_tf(self): 
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = "map"
+        t.child_frame_id = "base_link"
+
+        t.transform.translation.x = 0.5
+        t.transform.translation.y = 0.5
+        t.transform.translation.z = 0.0
+
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+
+        self.tf_broadcaster.sendTransform(t)
+
+    def timer_callback(self):
+        self.map.header.stamp = self.get_clock().now().to_msg()
+        self.map.data = self.grid.flatten().astype(np.int8).tolist()
+        self.map_pub.publish(self.map)
+
+
+def main(args=None):  # type: ignore
+    rclpy.init(args=args)
+    try:
+        test_map = TestMapGenerator()
+        rclpy.spin(test_map)
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
